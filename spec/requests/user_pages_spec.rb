b139b717455a266f15b9dfbc5a7269af71fc9bfd
Lutsko Michael <lutsko86@gmail.com>
2015-11-02 15:44:44 +0200

    Add  spec application helper and refactoring tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 describe 'User pages' do
 
   subject { page }
+  shared_examples_for 'all static pages' do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
-  describe 'signup page' do
+  describe 'Signup page' do
     before { visit signup_path }
+    let(:heading)    { 'Sign up' }
+    let(:page_title) { 'Sign up' }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it_should_behave_like 'all static pages'
+    it { should have_title('| Sign up') }
   end
 end
